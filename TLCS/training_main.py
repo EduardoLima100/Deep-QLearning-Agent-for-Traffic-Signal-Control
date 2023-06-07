25fa534770ba0298f4201eb53c2bd4e788e6acec
EduardoLima100 <eduardolima100@gmail.com>
2023-06-07 09:21:53 -0300

    Update train_main.py

--- TLCS/training_main.py
+++ TLCS/training_main.py
@@ -1,5 +1,4 @@
-from __future__ import absolute_import
-from __future__ import print_function
+from __future__ import absolute_import, print_function
 
 import os
 import datetime
@@ -12,21 +11,33 @@ from model import TrainModel
 from visualization import Visualization
 from utils import import_train_configuration, set_sumo, set_train_path
 
-
 if __name__ == "__main__":
 
     config = import_train_configuration(config_file='training_settings.ini')
     sumo_cmd = set_sumo(config['gui'], config['sumocfg_file_name'], config['max_steps'])
-    path = set_train_path(config['models_path_name'])
+    path = set_train_path(config['models_path_name'], config['load_existing_model'], config['pretrained_model_number'])
 
-    Model = TrainModel(
-        config['num_layers'], 
-        config['width_layers'], 
-        config['batch_size'], 
-        config['learning_rate'], 
-        input_dim=config['num_states'], 
-        output_dim=config['num_actions']
-    )
+
+    if config['load_existing_model']:
+        model_path = os.path.join(config['models_path_name'], 'model_' + str(config['pretrained_model_number']))
+        Model = TrainModel(
+            config['num_layers'], 
+            config['width_layers'], 
+            config['batch_size'], 
+            config['learning_rate'], 
+            input_dim=config['num_states'], 
+            output_dim=config['num_actions'],
+            model_path=model_path
+        )
+    else:
+        Model = TrainModel(
+            config['num_layers'], 
+            config['width_layers'], 
+            config['batch_size'], 
+            config['learning_rate'], 
+            input_dim=config['num_states'], 
+            output_dim=config['num_actions']
+        )
 
     Memory = Memory(
         config['memory_size_max'], 
@@ -52,6 +63,7 @@ if __name__ == "__main__":
         config['max_steps'],
         config['green_duration'],
         config['yellow_duration'],
+        config['red_duration'],
         config['num_states'],
         config['num_actions'],
         config['training_epochs']
@@ -62,7 +74,8 @@ if __name__ == "__main__":
     
     while episode < config['total_episodes']:
         print('\n----- Episode', str(episode+1), 'of', str(config['total_episodes']))
-        epsilon = 1.0 - (episode / config['total_episodes'])  # set the epsilon for this episode according to epsilon-greedy policy
+        epsilon = (1 - (episode / config['total_episodes'])) * 0.2  # set the epsilon for this episode according to epsilon-greedy policy
+        epsilon = epsilon if epsilon > 0.01 else 0.01
         simulation_time, training_time = Simulation.run(episode, epsilon)  # run the simulation
         print('Simulation time:', simulation_time, 's - Training time:', training_time, 's - Total:', round(simulation_time+training_time, 1), 's')
         episode += 1
