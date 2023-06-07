0c988fd0f257cd3d2d9407e160d055bfe4e56b21
EduardoLima100 <eduardolima100@gmail.com>
2023-06-07 15:51:31 -0300

    updates

--- TLCS/training_main.py
+++ TLCS/training_main.py
@@ -74,9 +74,9 @@ if __name__ == "__main__":
     
     while episode < config['total_episodes']:
         print('\n----- Episode', str(episode+1), 'of', str(config['total_episodes']))
-        epsilon = (1 - (episode / config['total_episodes'])) * 0.2  # set the epsilon for this episode according to epsilon-greedy policy
+        epsilon = (1 - (episode / config['total_episodes'])) * 1  # set the epsilon for this episode according to epsilon-greedy policy
         epsilon = epsilon if epsilon > 0.01 else 0.01
-        simulation_time, training_time = Simulation.run(episode, epsilon)  # run the simulation
+        simulation_time, training_time = Simulation.run(epsilon)  # run the simulation
         print('Simulation time:', simulation_time, 's - Training time:', training_time, 's - Total:', round(simulation_time+training_time, 1), 's')
         episode += 1
 
